74f0d2a31323beb29a1501a1e5b8aa119e6b3301
Justin Bitely <jbitely@gmail.com>
2016-02-09 15:36:59 -0500

    Stopped for lunch. PW hashing but DB saving unhashed.

--- app/models/user.js
+++ app/models/user.js
@@ -5,10 +5,14 @@ var Promise = require('bluebird');
 
 var User = db.Model.extend({
   tableName: 'users',
+  password: '',
   initialize: function(){
     this.on('creating', function(model, attrs, op){
       bcrypt.hash(model.get('password'), null, null, function(err, hash){
         model.set('password', hash);
+
+        console.log('model is ' + model.password);
+        console.log('hash is ' + hash);
       });
     })
   },
